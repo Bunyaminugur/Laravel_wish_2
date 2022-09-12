4d4dc0e6426c080bdc7705ae72945eb4a96f637e
ifayyad <fayyadan@gmail.com>
2022-09-12 12:02:04 +0200

    added pagination

--- resources/js/Pages/Communities/Index.vue
+++ resources/js/Pages/Communities/Index.vue
@@ -33,7 +33,7 @@
                                         </tr>
                                         </thead>
                                         <tbody class="divide-y divide-gray-200 bg-white">
-                                        <tr v-for="community in communities" :key="community.id">
+                                        <tr v-for="community in communities.data" :key="community.id">
                                             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ community.name}}</td>
                                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ community.slug}}</td>
                                             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
@@ -43,6 +43,11 @@
                                         </tr>
                                         </tbody>
                                     </table>
+
+                                    <div class="m-2 p-2">
+                                        <Pagination :links="communities.links"/>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -59,6 +64,7 @@
 
   import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
   import { Head, Link } from '@inertiajs/inertia-vue3';
+  import Pagination from "../../Components/Pagination.vue";
 
   defineProps({
     communities: Object
