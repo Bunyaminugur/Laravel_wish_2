9738c0295eb1d7b14edd6f917a9a054ea35497f2
Bunyaminugur <bunyamin26@outlook.com>
2022-09-23 11:30:13 +0200

    Add Communitiy sidebar to welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Inertia\Inertia;
+use App\Models\Community;
 use Illuminate\Http\Request;
+use App\Http\Resources\CommunityResource;
 use App\Http\Resources\CommunityPostResource;
 
 class WelcomeController extends Controller
@@ -15,6 +17,8 @@ class WelcomeController extends Controller
             $query->where('user_id', auth()->id());
         }])->withCount('comments')->withCount('comments')->orderBy('votes', 'desc')->take(12)->get());
 
-        return Inertia::render('Welcome', compact('posts'));
+        $communities = CommunityResource::collection(Community::withCount('posts')->orderBy('posts_count', 'desc')->take(6)->get());
+
+        return Inertia::render('Welcome', compact('posts', 'communities'));
     }
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,8 +5,9 @@
                 <PostCard v-for="post in posts.data" :post="post" :community="post.community_slug" :key="post.id" />
             </div>
             <div class="w-4/12 p-3">
-                <div class="m-2 p-2 bg-slate-600 text-white rounded-md">
+                <div class="m-2 p-2  rounded-md">
                     <h2>Top Communities</h2>
+                    <CommunityList :communities="communities.data" />
                 </div>
             </div>
         </section>
@@ -16,6 +17,7 @@
 <script setup>
 import PostCard from '@/Components/PostCard.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import CommunityList from '@/Components/CommunityList.vue';
 
 defineProps({
     communities: Object,
